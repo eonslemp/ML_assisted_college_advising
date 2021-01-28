ccaba239809d5ca04ab2159c3648e00057a70cd0
Eon SLemp <eonslemp@gmail.com>
2021-01-28 18:11:53 -0500

    final

--- code/functions.py
+++ code/functions.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split
 
